97da654b63f1d60e40e7cc5dbded8db9c6cc0fd3
paul <paul.chazalmartin@protonmail.com>
2026-07-09 09:53:12 +0200

    Add +\r\n to msg in reply/reply.cpp sendToChanel()

--- src/reply/Reply.cpp
+++ src/reply/Reply.cpp
@@ -16,7 +16,8 @@ void Server::sendReply(Client &client, const std::string &code, const std::strin
 void Server::sendToChannel(const Channel &chan, std::string &str)
 {
 	std::map<std::string, Client *> membres = chan.getMembres();
+	std::string						msg		= str + "\r\n";
 	for (CliIt it = membres.begin(); it != membres.end(); it++) {
-		send(it->second->getFd(), str.c_str(), str.size(), 0);
+		send(it->second->getFd(), msg.c_str(), msg.size(), 0);
 	}
 }
