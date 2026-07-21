7d476697e697e9e401c5f905abc510d46cced03a
paul <paul.chazalmartin@protonmail.com>
2026-07-21 07:19:40 +0200

    Join rply notopic if no topic

--- src/server/commands/join.cpp
+++ src/server/commands/join.cpp
@@ -10,58 +10,61 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../Server.hpp"
 #include "../../reply/Reply.hpp"
+#include "../Server.hpp"
 #include <new>
 
 void Server::join(Client &client, std::vector<std::string> &params)
 {
-    if (!requireRegistered(client))
-        return;
-    if (!requireParams(client, params, 1, "JOIN"))
-        return;
-    if (params[0].empty() || params[0][0] != '#') {
-        sendReply(client, ERR_NOSUCHCHANNEL, params[0] + " :No such channel");
-        return;
-    }
+	if (!requireRegistered(client))
+		return;
+	if (!requireParams(client, params, 1, "JOIN"))
+		return;
+	if (params[0].empty() || params[0][0] != '#') {
+		sendReply(client, ERR_NOSUCHCHANNEL, params[0] + " :No such channel");
+		return;
+	}
 
-    std::string key = (params.size() > 1) ? params[1] : "";
-    Channel       *chan;
+	std::string key = (params.size() > 1) ? params[1] : "";
+	Channel	   *chan;
 
-    if (findChannelToLst(params[0])) {
-        chan = requireChannel(client, params[0]);
-        if (!chan)
-            return;
-        if (chan->isInviteOnly() && !chan->isInvited(client)) {
-            sendReply(client, ERR_INVITEONLYCHAN, chan->getName() + " :Cannot join channel (+i)");
-            return;
-        }
-        if (chan->hasLimit() && chan->memberCount() >= chan->getLimit()) {
-            sendReply(client, ERR_CHANNELISFULL, chan->getName() + " :Cannot join channel (+l)");
-            return;
-        }
-        if (chan->hasKey() && !chan->keyIsValid(key)) {
-            sendReply(client, ERR_BADCHANNELKEY, chan->getName() + " :Cannot join channel (+k)");
-            return;
-        }
-    } else {
-        try {
-            chan = new Channel(params[0]);
-        } catch (const std::bad_alloc &) {
-            return;
-        }
-        addChannelToLst(*chan);
-        chan->addMember(client);
-        chan->addModerator(client);
-        goto end;
-    }
+	if (findChannelToLst(params[0])) {
+		chan = requireChannel(client, params[0]);
+		if (!chan)
+			return;
+		if (chan->isInviteOnly() && !chan->isInvited(client)) {
+			sendReply(client, ERR_INVITEONLYCHAN, chan->getName() + " :Cannot join channel (+i)");
+			return;
+		}
+		if (chan->hasLimit() && chan->memberCount() >= chan->getLimit()) {
+			sendReply(client, ERR_CHANNELISFULL, chan->getName() + " :Cannot join channel (+l)");
+			return;
+		}
+		if (chan->hasKey() && !chan->keyIsValid(key)) {
+			sendReply(client, ERR_BADCHANNELKEY, chan->getName() + " :Cannot join channel (+k)");
+			return;
+		}
+	} else {
+		try {
+			chan = new Channel(params[0]);
+		} catch (const std::bad_alloc &) {
+			return;
+		}
+		addChannelToLst(*chan);
+		chan->addMember(client);
+		chan->addModerator(client);
+		goto end;
+	}
 
-    chan->addMember(client);
+	chan->addMember(client);
 end:
-    sendToChannel(*chan,
-                  ":" + client.getNickname() + "!" + client.getUsername() +
-                      "@localhost JOIN :" + chan->getName());
-    sendReply(client, RPL_TOPIC, chan->getName() + " :" + chan->getTopic());
-    sendReply(client, RPL_NAMREPLY, "= " + chan->getName() + " :" + chan->getMembersList());
-    sendReply(client, RPL_ENDOFNAMES, chan->getName() + " :End of /NAMES list");
+	sendToChannel(*chan,
+				  ":" + client.getNickname() + "!" + client.getUsername() +
+					  "@localhost JOIN :" + chan->getName());
+	if (chan->getTopic().empty())
+		sendReply(client, RPL_NOTOPIC, chan->getName() + " :No topic is set");
+	else
+		sendReply(client, RPL_TOPIC, chan->getName() + " :" + chan->getTopic());
+	sendReply(client, RPL_NAMREPLY, "= " + chan->getName() + " :" + chan->getMembersList());
+	sendReply(client, RPL_ENDOFNAMES, chan->getName() + " :End of /NAMES list");
 }
